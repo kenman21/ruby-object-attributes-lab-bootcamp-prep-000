8aa08479bf9869519097bbdb4630f3779e8a5bdd
Kenneth Lehr <kenneth.c.lehr@gmail.com>
2018-01-07 20:29:09 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,20 @@
 class Dog 
-  def initialize(name)
+  def initialize(name, breed)
     @name = name
   end 
+  
+  def name
+    @name
+  end 
+  
   
   def name=(new_name)
     @name = new_name
   end 
   
+  def breed
+    @breed
+  end 
   
   
   def breed=(new_breed)
