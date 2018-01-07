5cfcd23dfcdf2ee51ecee459fd4b2868c986cde2
Kenneth Lehr <kenneth.c.lehr@gmail.com>
2018-01-07 20:28:32 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,5 +3,14 @@ class Dog
     @name = name
   end 
   
-  def name=
-    @name = 
+  def name=(new_name)
+    @name = new_name
+  end 
+  
+  
+  
+  def breed=(new_breed)
+    @breed = new_breed
+  end 
+  
+  
